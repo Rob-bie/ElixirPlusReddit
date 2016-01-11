99eda143b900677e548626f6b45e49d6a8139c96
Rob-bie <deibbor@gmail.com>
2016-01-10 22:03:09 -0500

    PQueue docs updated

--- lib/elixirplusreddit/pqueue.ex
+++ lib/elixirplusreddit/pqueue.ex
@@ -1,16 +1,36 @@
 defmodule ElixirPlusReddit.PQueue do
 
+  @moduledoc """
+  A tiny wrapper around okeuday's pqueue library.
+  """
+
+  @doc """
+  Create a new pqueue.
+  """
+
   def new do
     :pqueue.new
   end
+
+  @doc """
+  Insert a request into a pqueue.
+  """
 
   def enqueue(pqueue, request, priority) do
     :pqueue.in(request, priority, pqueue)
   end
 
+  @doc """
+  Return the next request in the pqueue.
+  """
+
   def dequeue(pqueue) do
     :pqueue.out(pqueue)
   end
+
+  @doc """
+  Return if the pqueue is empty.
+  """
 
   def is_empty?(pqueue) do
     :pqueue.is_empty(pqueue)
