e1ca6ab672659cae794afa39bb62d93b1b2b3c62
Rob-bie <deibbor@gmail.com>
2016-01-10 22:00:38 -0500

    Added utility for accessing configuration data

--- lib/elixirplusreddit/config.ex
+++ lib/elixirplusreddit/config.ex
@@ -1,3 +1,31 @@
 defmodule ElixirPlusReddit.Config do
-  
+
+  @moduledoc """
+  An interface for accessing configuration data from config.exs.
+  """
+
+  @doc """
+  Return credentials from config.exs. Credentials are stored as a
+  keyword list:
+
+  [
+  username:      username,
+  password:      password,
+  client_id:     client_id,
+  client_secret: client_secret
+  ]
+  """
+
+  def credentials do
+    Application.get_env(:elixirplusreddit, :creds)
+  end
+
+  @doc """
+  Return the client's user agent.
+  """
+
+  def user_agent do
+    Application.get_env(:elixirplusreddit, :user_agent)
+  end
+
 end
