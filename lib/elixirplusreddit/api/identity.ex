efc7ee953b709a4cedd0797154202352ac4c7f96
Rob-bie <deibbor@gmail.com>
2016-01-10 22:06:24 -0500

    Some endpoints for accessing information about self implemented

--- lib/elixirplusreddit/api/identity.ex
+++ lib/elixirplusreddit/api/identity.ex
@@ -1,3 +1,29 @@
 defmodule ElixirPlusReddit.API.Identity do
-  
+
+  @moduledoc """
+  An interface for getting information about the authenticated user.
+  """
+
+  alias ElixirPlusReddit.RequestServer
+  alias ElixirPlusReddit.RequestBuilder
+
+  @identity_base "https://oauth.reddit.com/api/v1/me"
+
+  def me(from, tag, priority \\ 0) do
+    request_data = RequestBuilder.format_get(from, tag, @identity_base, :ok, priority)
+    RequestServer.enqueue_request(request_data)
+  end
+
+  def prefs(from, tag, priority \\ 0) do
+    url = "#{@identity_base}/prefs"
+    request_data = RequestBuilder.format_get(from, tag, url, :ok, priority)
+    RequestServer.enqueue_request(request_data)
+  end
+
+  def trophies(from, tag, priority \\ 0) do
+    url = "#{@identity_base}/trophies"
+    request_data = RequestBuilder.format_get(from, tag, url, :ok, priority)
+    RequestServer.enqueue_request(request_data)
+  end
+
 end
