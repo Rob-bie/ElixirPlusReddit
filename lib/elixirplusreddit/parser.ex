1646056300a27abae862c9f9f309e983fc56d91b
Rob-bie <deibbor@gmail.com>
2016-01-10 22:01:40 -0500

    Very early workings of a parser added

--- lib/elixirplusreddit/parser.ex
+++ lib/elixirplusreddit/parser.ex
@@ -1,3 +1,24 @@
 defmodule ElixirPlusReddit.Parser do
 
+  @moduledoc """
+  A parser built with Poison for decoding Reddit's various return
+  structures.
+  """
+
+  def parse(%HTTPotion.Response{body: body, status_code: code}, strategy) do
+    case code do
+      _ ->
+        body = Poison.decode!(body, keys: :atoms)
+        parse(body, strategy)
+    end
+  end
+
+  def parse(resp, :token) do
+    "bearer #{resp.access_token}"
+  end
+
+  def parse(resp, _) do
+    resp
+  end
+
 end
