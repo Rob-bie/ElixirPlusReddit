bef123cae05168d3aedeb465f968567284a6e5d1
Rob-bie <deibbor@gmail.com>
2016-01-10 22:06:39 -0500

    Servers added to supervision tree

--- lib/elixirplusreddit.ex
+++ lib/elixirplusreddit.ex
@@ -1,10 +1,15 @@
 defmodule ElixirPlusReddit do
   use Application
 
+  alias ElixirPlusReddit.RequestServer
+  alias ElixirPlusReddit.TokenServer
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
+      worker(RequestServer, []),
+      worker(TokenServer, [])
     ]
 
     opts = [strategy: :one_for_one, name: ElixirPlusReddit.Supervisor]
