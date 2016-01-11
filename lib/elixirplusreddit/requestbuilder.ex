d03015fc46284e5ee8693cbe93e73823b8191028
Rob-bie <deibbor@gmail.com>
2016-01-10 22:01:21 -0500

    Added utility for building requests

--- lib/elixirplusreddit/requestbuilder.ex
+++ lib/elixirplusreddit/requestbuilder.ex
@@ -1,3 +1,43 @@
 defmodule ElixirPlusReddit.RequestBuilder do
-  
+
+  @moduledoc """
+  A utility module for formatting request data.
+  """
+
+  def format_get(from, tag, url, parse_strategy, priority) do
+    %{
+      method: :get,
+      from: from,
+      tag: tag,
+      url: url,
+      parse_strategy: parse_strategy,
+      priority: priority
+    }
+  end
+
+
+  def format_get(from, tag, url, query, parse_strategy, priority) do
+    %{
+      method: :get,
+      from: from,
+      tag: tag,
+      url: url,
+      query: query,
+      parse_strategy: parse_strategy,
+      priority: priority
+    }
+  end
+
+  def format_post(from, tag, url, query, parse_strategy, priority) do
+    %{
+      method: :post,
+      from: from,
+      tag: tag,
+      url: url,
+      query: query,
+      parse_strategy: parse_strategy,
+      priority: priority
+    }
+  end
+
 end
