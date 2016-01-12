0eac8026b1d5a30d9ec84ac257eae0430f132f28
Rob-bie <deibbor@gmail.com>
2016-01-11 23:20:45 -0500

    Access to some subreddit based endpoints added

--- lib/elixirplusreddit/api/subreddit.ex
+++ lib/elixirplusreddit/api/subreddit.ex
@@ -1,3 +1,91 @@
 defmodule ElixirPlusReddit.API.Subreddit do
-  
+
+  @moduledoc """
+  An interface for getting subreddit information.
+  """
+
+  alias ElixirPlusReddit.RequestServer
+  alias ElixirPlusReddit.RequestBuilder
+
+  @subreddit_base  "https://oauth.reddit.com/r"
+  @default_priority 0
+
+  def hot(from, tag, subreddit, options, priority) do
+    listing(from, tag, subreddit, options, :hot, priority)
+  end
+
+  def hot(from, tag, subreddit, options) when is_list(options) do
+    listing(from, tag, subreddit, [], :hot, @default_priority)
+  end
+
+  def hot(from, tag, subreddit, priority) do
+    listing(from, tag, subreddit, [], :hot, priority)
+  end
+
+  def new(from, tag, subreddit, options, priority) do
+    listing(from, tag, subreddit, options, :new, priority)
+  end
+
+  def new(from, tag, subreddit, options) when is_list(options) do
+    listing(from, tag, subreddit, [], :new, @default_priority)
+  end
+
+  def new(from, tag, subreddit, priority) do
+    listing(from, tag, subreddit, [], :new, priority)
+  end
+
+  def rising(from, tag, subreddit, options, priority) do
+    listing(from, tag, subreddit, options, :rising, priority)
+  end
+
+  def rising(from, tag, subreddit, options) when is_list(options) do
+    listing(from, tag, subreddit, [], :rising, @default_priority)
+  end
+
+  def rising(from, tag, subreddit, priority) do
+    listing(from, tag, subreddit, [], :rising, priority)
+  end
+
+  def controversial(from, tag, subreddit, options, priority) do
+    listing(from, tag, subreddit, options, :controversial, priority)
+  end
+
+  def controversial(from, tag, subreddit, options) when is_list(options) do
+    listing(from, tag, subreddit, [], :controversial, @default_priority)
+  end
+
+  def controversial(from, tag, subreddit, priority) do
+    listing(from, tag, subreddit, [], :controversial, priority)
+  end
+
+  def top(from, tag, subreddit, options, priority) do
+    listing(from, tag, subreddit, options, :top, priority)
+  end
+
+  def top(from, tag, subreddit, options) when is_list(options) do
+    listing(from, tag, subreddit, [], :top, @default_priority)
+  end
+
+  def top(from, tag, subreddit, priority) do
+    listing(from, tag, subreddit, [], :top, priority)
+  end
+
+  def gilded(from, tag, subreddit, options, priority) do
+    listing(from, tag, subreddit, options, :gilded, priority)
+  end
+
+  def gilded(from, tag, subreddit, options) when is_list(options) do
+    listing(from, tag, subreddit, [], :gilded, @default_priority)
+  end
+
+  def gilded(from, tag, subreddit, priority) do
+    listing(from, tag, subreddit, [], :gilded, priority)
+  end
+
+  defp listing(from, tag, subreddit, options, endpoint, priority) do
+    url = "#{@subreddit_base}/#{subreddit}/#{endpoint}"
+    request_data = RequestBuilder.format_get(from, tag, url, options, :listing, priority)
+    RequestServer.enqueue_request(request_data)
+  end
+
 end
