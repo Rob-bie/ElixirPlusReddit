e4c10502ac9865ba73b81a53d4988cb535277577
Rob-bie <deibbor@gmail.com>
2016-01-19 01:40:03 -0500

    Streaming capabilities added to some user/subreddit listings

--- lib/elixirplusreddit/api/subreddit.ex
+++ lib/elixirplusreddit/api/subreddit.ex
@@ -6,9 +6,15 @@ defmodule ElixirPlusReddit.API.Subreddit do
 
   alias ElixirPlusReddit.RequestServer
   alias ElixirPlusReddit.RequestBuilder
+  alias ElixirPlusReddit.Scheduler
 
+  @subreddit       __MODULE__
   @subreddit_base  "https://oauth.reddit.com/r"
   @default_priority 0
+
+  def hot(from, tag, subreddit) do
+    listing(from, tag, subreddit, [], :hot, @default_priority)
+  end
 
   def hot(from, tag, subreddit, options, priority) do
     listing(from, tag, subreddit, options, :hot, priority)
@@ -20,6 +26,10 @@ defmodule ElixirPlusReddit.API.Subreddit do
 
   def hot(from, tag, subreddit, priority) do
     listing(from, tag, subreddit, [], :hot, priority)
+  end
+
+  def new(from, tag, subreddit) do
+    listing(from, tag, subreddit, [], :new, @default_priority)
   end
 
   def new(from, tag, subreddit, options, priority) do
@@ -34,6 +44,10 @@ defmodule ElixirPlusReddit.API.Subreddit do
     listing(from, tag, subreddit, [], :new, priority)
   end
 
+  def rising(from, tag, subreddit) do
+    listing(from, tag, subreddit, [], :rising, @default_priority)
+  end
+
   def rising(from, tag, subreddit, options, priority) do
     listing(from, tag, subreddit, options, :rising, priority)
   end
@@ -44,6 +58,10 @@ defmodule ElixirPlusReddit.API.Subreddit do
 
   def rising(from, tag, subreddit, priority) do
     listing(from, tag, subreddit, [], :rising, priority)
+  end
+
+  def controversial(from, tag, subreddit) do
+    listing(from, tag, subreddit, [], :controversial, @default_priority)
   end
 
   def controversial(from, tag, subreddit, options, priority) do
@@ -58,6 +76,10 @@ defmodule ElixirPlusReddit.API.Subreddit do
     listing(from, tag, subreddit, [], :controversial, priority)
   end
 
+  def top(from, tag, subreddit) do
+    listing(from, tag, subreddit, [], :top, @default_priority)
+  end
+
   def top(from, tag, subreddit, options, priority) do
     listing(from, tag, subreddit, options, :top, priority)
   end
@@ -68,6 +90,10 @@ defmodule ElixirPlusReddit.API.Subreddit do
 
   def top(from, tag, subreddit, priority) do
     listing(from, tag, subreddit, [], :top, priority)
+  end
+
+  def gilded(from, tag, subreddit) do
+    listing(from, tag, subreddit, [], :gilded, @default_priority)
   end
 
   def gilded(from, tag, subreddit, options, priority) do
@@ -82,6 +108,10 @@ defmodule ElixirPlusReddit.API.Subreddit do
     listing(from, tag, subreddit, [], :gilded, priority)
   end
 
+  def comments(from, tag, subreddit) do
+    listing(from, tag, subreddit, [], :comments, @default_priority)
+  end
+
   def comments(from, tag, subreddit, options, priority) do
     listing(from, tag, subreddit, options, :comments, priority)
   end
@@ -92,6 +122,38 @@ defmodule ElixirPlusReddit.API.Subreddit do
 
   def comments(from, tag, subreddit, priority) do
     listing(from, tag, subreddit, [], :comments, priority)
+  end
+
+  def stream_submissions(from, tag, subreddit, interval) do
+    Scheduler.schedule(from, tag, {@subreddit, :new}, [subreddit, [], @default_priority], interval)
+  end
+
+  def stream_submissions(from, tag, subreddit, options, interval) when is_list(options) do
+    Scheduler.schedule(from, tag, {@subreddit, :new}, [subreddit, options, @default_priority], interval)
+  end
+
+  def stream_submissions(from, tag, subreddit, priority, interval) do
+    Scheduler.schedule(from, tag, {@subreddit, :new}, [subreddit, [] , priority], interval)
+  end
+
+  def stream_submissions(from, tag, subreddit, options, priority, interval) do
+    Scheduler.schedule(from, tag, {@subreddit, :new}, [subreddit, options, priority], interval)
+  end
+
+  def stream_comments(from, tag, subreddit, interval) do
+    Scheduler.schedule(from, tag, {@subreddit, :comments}, [subreddit, [], @default_priority], interval)
+  end
+
+  def stream_comments(from, tag, subreddit, options, interval) when is_list(options) do
+    Scheduler.schedule(from, tag, {@subreddit, :comments}, [subreddit, options, @default_priority], interval)
+  end
+
+  def stream_comments(from, tag, subreddit, priority, interval) do
+    Scheduler.schedule(from, tag, {@subreddit, :comments}, [subreddit, [] , priority], interval)
+  end
+
+  def stream_comments(from, tag, subreddit, options, priority, interval) do
+    Scheduler.schedule(from, tag, {@subreddit, :comments}, [subreddit, options, priority], interval)
   end
 
   defp listing(from, tag, subreddit, options, endpoint, priority) do
--- lib/elixirplusreddit/api/user.ex
+++ lib/elixirplusreddit/api/user.ex
@@ -6,9 +6,15 @@ defmodule ElixirPlusReddit.API.User do
 
   alias ElixirPlusReddit.RequestBuilder
   alias ElixirPlusReddit.RequestServer
+  alias ElixirPlusReddit.Scheduler
 
+  @user      __MODULE__
   @user_base "https://oauth.reddit.com/user"
   @default_priority 0
+
+  def comments(from, tag, username) do
+    listing(from, tag, username, [], :comments, @default_priority)
+  end
 
   def comments(from, tag, username, options, priority) do
     listing(from, tag, username, options, :comments, priority)
@@ -20,6 +26,22 @@ defmodule ElixirPlusReddit.API.User do
 
   def comments(from, tag, username, priority) do
     listing(from, tag, username, [], :comments, priority)
+  end
+
+  def stream_comments(from, tag, username, interval) do
+    Scheduler.schedule(from, tag, {@user, :comments}, [username, [], @default_priority], interval)
+  end
+
+  def stream_comments(from, tag, username, options, interval) when is_list(options) do
+    Scheduler.schedule(from, tag, {@user, :comments}, [username, options, @default_priority], interval)
+  end
+
+  def stream_comments(from, tag, username, priority, interval) do
+    Scheduler.schedule(from, tag, {@user, :comments}, [username, [] , priority], interval)
+  end
+
+  def stream_comments(from, tag, username, options, priority, interval) do
+    Scheduler.schedule(from, tag, {@user, :comments}, [username, options, priority], interval)
   end
 
   defp listing(from, tag, username, options, endpoint, priority) do
