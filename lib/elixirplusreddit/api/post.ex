182e21d6235af76fac487c2e4a32a1d6c2fd492a
Rob-bie <deibbor@gmail.com>
2016-01-20 13:08:52 -0500

    Tiny changes to post

--- lib/elixirplusreddit/api/post.ex
+++ lib/elixirplusreddit/api/post.ex
@@ -8,8 +8,9 @@ defmodule ElixirPlusReddit.API.Post do
   alias ElixirPlusReddit.RequestBuilder
 
   @comment_endpoint "https://oauth.reddit.com/api/comment"
+  @default_priority  0
 
-  def comment(from, tag, id, text, priority \\ 0) do
+  def comment(from, tag, id, text, priority \\ @default_priority) do
     query = [api_type: :json, text: text, thing_id: id]
     request_data = RequestBuilder.format_post(from, tag, @comment_endpoint, query, :ok, priority)
     RequestServer.enqueue_request(request_data)
