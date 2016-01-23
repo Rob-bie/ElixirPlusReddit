1fd49fe53d9183d538f11f36d854a9936daa535a
Rob-bie <deibbor@gmail.com>
2016-01-22 22:01:29 -0500

    Documented single post function

--- lib/elixirplusreddit/api/post.ex
+++ lib/elixirplusreddit/api/post.ex
@@ -10,9 +10,57 @@ defmodule ElixirPlusReddit.API.Post do
   @comment_endpoint "https://oauth.reddit.com/api/comment"
   @default_priority  0
 
-  def comment(from, tag, id, text, priority \\ @default_priority) do
+  @doc """
+  Reply to a comment or submission.
+
+  ### Parameters
+
+  * `from`:      The pid or name of the requester.
+  * `tag`:       Anything.
+  * `id`:        A comma separated list of message ids.
+  * `text`:      The reply body.
+  * `priority`:  The request's priority. (Optional)
+
+  ### Fields
+      distinguished
+      saved
+      replies
+      link_id
+      report_reasons
+      likes
+      banned_by
+      subreddit_id
+      body_html
+      archived
+      gilded
+      score_hidden
+      body
+      created_utc
+      ups
+      stickied
+      edited
+      num_reports
+      mod_reports
+      approved_by
+      errors
+      user_reports
+      name
+      controversiality
+      parent_id
+      removal_reason
+      author_flair_text
+      created
+      id
+      downs
+      author
+      subreddit
+      author_flair_css_class
+      score
+  """
+
+  def reply(from, tag, id, text, priority \\ @default_priority) do
     query = [api_type: :json, text: text, thing_id: id]
-    request_data = RequestBuilder.format_post(from, tag, @comment_endpoint, query, :ok, priority)
+    request_data = RequestBuilder.format_post(from, tag, @comment_endpoint, query, :reply, priority)
     RequestServer.enqueue_request(request_data)
   end
   
